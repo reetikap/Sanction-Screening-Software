be27be8aceacc47dd4c1366c793ba9856ececd98
reetikap <rpoosarla@gmail.com>
2020-03-29 12:50:13 +0530

    Initial commit

---
+++ src/main/java/com/cbp/SanctioningApplication.java
@@ -0,0 +1,13 @@
+package com.cbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanctioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanctioningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbp/SanctioningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanctioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
